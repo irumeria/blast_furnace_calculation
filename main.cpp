df458d8a034b7c1d3c2dfc9bdd0f196973946537
irumeria4@gmail.com <irumeria4@gmail.com>
2022-04-07 09:17:49 +0800

    add license

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ using std::string;
 int main() {
 
   // 第一张配料单所使用的输入参数
-  // string filename("input1.json"); //读取输入文件 -- 第一张
+  // string filename("./data/input1.json"); //读取输入文件 -- 第一张
   // vector<string> ores_name = {"sinter", "aust", "pellet"}; // 使用的矿名
   // vector<string> equ_elements = {"TFe", "Mn", "P"}; // 配比计算使用的元素
 
-  //  第二张配料单所使用的输入参数
-  string filename("input2.json"); //读取输入文件 -- 第二张
+  // 第二张配料单所使用的输入参数
+  string filename("./data/input2.json"); //读取输入文件 -- 第二张
   vector<string> ores_name = {"sinter", "aust"}; // 使用的矿名
   vector<string> equ_elements = {"TFe", "P"};   // 配比计算使用的元素
 
